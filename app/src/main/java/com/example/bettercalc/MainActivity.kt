3cc54294b053859c8e249282d288442800da419a
kotvaldo <adamstano5@gmail.com>
2023-08-22 11:19:30 +0200

    FinalXML

--- app/src/main/java/com/example/bettercalc/MainActivity.kt
+++ app/src/main/java/com/example/bettercalc/MainActivity.kt
@@ -3,9 +3,15 @@ package com.example.bettercalc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
+
+    private var result = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
