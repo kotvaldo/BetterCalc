5688176a2c69caec70a1d630d4842e82591c7bc9
kotvaldo <95977497+kotvaldo@users.noreply.github.com>
2023-09-26 18:55:16 +0200

    mutiLayout

--- app/src/main/java/com/example/bettercalc/MainActivity.kt
+++ app/src/main/java/com/example/bettercalc/MainActivity.kt
@@ -6,12 +6,29 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.bettercalc.Fragments.Calculator
 import com.example.bettercalc.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val homeFragment = Calculator()
+        replaceFragment(homeFragment)
+
+        
+
+    }
+
+    private fun replaceFragment(fragment:Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container,fragment).commit()
 
     }
 }
